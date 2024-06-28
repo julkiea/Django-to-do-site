6c601825f90b6ca11bbefa019b65e985f77900b9
Liliana Bassa <lilianabassa0510@gmail.com>
2024-06-28 13:36:01 +0200

    Unable logging in

--- website/views.py
+++ website/views.py
@@ -1,4 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html', {})
+    
+    # Check if logging in
+    if request.method == "POST":
+        
+        # Get username and password:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authenticate user
+        user = authenticate(request, username = username, password = password)
+
+        # If authenticating is success
+        if user is not None:
+
+            # Log in 
+            login(request, user)
+             
+            # Display message 
+            messages.success(request, 'You have been logged in successfully')
+
+            # Redirect home
+            return redirect('home')
+        
+        # If authenticating is not success
+        else:
+            messages.success(request, 'An error occurred while logging in, please try again!')
+            
+            # Redirect home
+            return redirect('home')
+    
+    # If not logging in
+    else:
+
+        # Render home page
+        return render(request, 'home.html', {})
+
+
