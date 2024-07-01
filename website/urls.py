791cb0c80444c53b47df5566a599f486eded430a
Liliana Bassa <lilianabassa0510@gmail.com>
2024-07-01 12:49:20 +0200

    Add new url: login

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('logout/', views.logout_user, name = 'logout'),
-    path('register/', views.register_user, name = 'register')
+    path('register/', views.register_user, name = 'register'),
+    path('login/', views.login_user, name = 'login'),
 ]
