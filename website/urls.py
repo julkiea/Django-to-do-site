4f770d36a7cc211567c3d5520eac7198a8e23934
Liliana Bassa <lilianabassa0510@gmail.com>
2024-07-01 17:07:45 +0200

    Add a new url: add_task

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('logout/', views.logout_user, name = 'logout'),
     path('register/', views.register_user, name = 'register'),
     path('login/', views.login_user, name = 'login'),
+    path('add_task/', views.add_task, name = 'add_task'),
 ]
