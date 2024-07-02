9288c3f0577d4b8eead8ac04e5a9ca063e587bf4
Liliana Bassa <lilianabassa0510@gmail.com>
2024-07-02 11:29:21 +0200

    Add new url: delete

--- website/urls.py
+++ website/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('register/', views.register_user, name = 'register'),
     path('login/', views.login_user, name = 'login'),
     path('add_task/', views.add_task, name = 'add_task'),
-    path('task/<int:pk>/', views.task, name='task')
+    path('task/<int:pk>/', views.task, name='task'),
+    path('task/<int:pk>/delete/', views.delete_task, name='delete_task'),
 ]
